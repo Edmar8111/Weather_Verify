f658b3158b5b60f57bd1a9df06c3b9e856b8e514
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2024-11-18 12:55:35 -0300

    Delete api_rest directory

--- api_rest/home_api/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import path
-from . import views
-
-# Create your tests here.
-urlpatterns = [
-    path('home', views.valores_api.as_view(), name='home'),
-    path('home0', views.home_0, name='home0')
-]
