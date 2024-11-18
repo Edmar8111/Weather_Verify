9928740eadf13f86335a196742bab0bcee61c463
Edmar D. O. Filho <87383421+Edmar8111@users.noreply.github.com>
2024-11-18 17:42:00 -0300

    Delete api_rest directory

--- api_rest/home_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import valores
-
-class valoresSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = valores
-        fields = '__all__'
-        #fields = ('nome_cidade', 'sigla_estado')
